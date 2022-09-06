92614569010599c8af79faf8a64c97b55247d893
pier <ppattitoni@gmail.com>
2022-09-06 18:12:10 +0200

    Update: Tue Sep  6 06:12:10 PM CEST 2022

--- mindeb.sh
+++ mindeb.sh
@@ -10,37 +10,41 @@
 
 # Start with a minimal debian installation
 
-# Basic system stuff
-sudo apt install man wget
+# System utilities
+sudo apt install -y man wget
+
+# Microcode for AMD/Intel
+# sudo apt install -y amd64-microcode
+# sudo apt install -y intel-microcode 
 
 # Window manager
-sudo apt install i3
+sudo apt install -y i3
  
 # Window manager rice
-sudo apt install feh picom lxappearance fonts-font-awesome 
+sudo apt install -y feh picom lxappearance fonts-font-awesome 
 
 # Display manager
-sudo apt install lightdm slick-greeter lightdm-settings 
+sudo apt install -y lightdm slick-greeter lightdm-settings 
 
 # Audio
-sudo apt install pulseaudio alsa-utils pavucontrol
+sudo apt install -y pulseaudio alsa-utils pavucontrol pasystray
 
 # Notifications
-sudo apt install dunst libnotify-bin 
+sudo apt install -y dunst libnotify-bin 
 
 # System-tray applets
-sudo apt install flameshot diodon network-manager-gnome 
+sudo apt install -y flameshot diodon network-manager-gnome 
 
 # Terminal
-sudo apt install kitty
+sudo apt install -y kitty
 
 # File manager
-sudo apt install thunar gvfs-backends gvfs-fuse 
+sudo apt install -y thunar gvfs-backends gvfs-fuse 
 
 # Text editor 
-sudo apt install geany geany-common 
+sudo apt install -y geany geany-common 
 
 # Browser
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable*
+sudo apt install -y ./google-chrome-stable*
 rm google-chrome-stable*
